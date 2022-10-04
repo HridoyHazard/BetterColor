d6c356ad6c7c3523319febe1be715a1a13e57f56
Gaurav Kabra <gauravkab07@gmail.com>
2022-10-04 17:27:32 +0530

    Improved readability for message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -73,21 +73,23 @@ export default function Home() {
                 </button>
               </div>
               <div
-                className="bg"
-                style={{ backgroundColor: `${background}`, color: `${color}` }}>
-                <h2 id="one">Read Below Message</h2>
-                <p id="two">
-                  When you work for peace or any other aspect of social change,
-                  there are often hardships to overcome. You must believe deeply
-                  that what you are doing is right, or else you may become
-                  discouraged and give up. I have found that there are no easy
-                  solutions to problems involving social change. When you commit
-                  yourself to creating a better world, you are most likely
-                  committing yourself to a lifetime of effort.
-                </p>
-                <p id="message" style = {{color : `${invertColor(background)}`}}>
-                  Can You Read The Message Above Comfortably With This Background? If
-                  Yes, Then Copy The Color Code. Else Try Changing Colors.
+                className="bg">
+                <div className="text" style={{ backgroundColor: `${background}`, color: `${color}` }}>
+                  <h2 id="one">Read This Message</h2>
+                  <p id="two">
+                    When you work for peace or any other aspect of social
+                    change, there are often hardships to overcome. You must
+                    believe deeply that what you are doing is right, or else you
+                    may become discouraged and give up. I have found that there
+                    are no easy solutions to problems involving social change.
+                    When you commit yourself to creating a better world, you are
+                    most likely committing yourself to a lifetime of effort.
+                  </p>
+                </div>
+                <p id="message">
+                  Can You Read The Message Above Comfortably With This
+                  Background? If Yes, Then Copy The Color Code. Else Try
+                  Changing Colors.
                 </p>
               </div>
             </div>
