86e89058e1dbefe3e055cef39239e8b5ce129b47
jsapuyesp <jsapuyesp@gmail.com>
2022-10-04 19:13:31 -0500

    first changes in header and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,114 +21,110 @@ export default function Home() {
   };
 
   return (
-    <div className="main-container">
-      <div className="cont">
-        <header className="header">
-          <div className="upper">
-            <h2>BetterColor</h2>
+    <>
+      <header className="header">
+        <br />
+        <h1>BetterColor</h1>
+        <br />
+        <p>
+          A Website For Finding Better Color Combinations For Readability
+        </p>
+        <br />
+      </header>
+
+      <div className="main">
+        <div className="click">
+          <div className="left_code">
+            <div className="left">
+              <p>Select Color For Text:</p>
+              <input
+                type="color"
+                id="text"
+                defaultValue="#000000"
+                onChange={(e) => setColor(e.target.value)}
+              />
+            </div>
+            <div className="hex_code">
+              <p>{color}</p>
+            </div>
+            <button onClick={() => navigator.clipboard.writeText(color)}>
+              Copy Me!
+            </button>
           </div>
-          <div className="mainBox">
-            <div className="main">
-              <p>
-                A Website For Finding Better Color Combinations For Readability
-                <br />
-                <br />
+          <div className="right_code">
+            <div className="right">
+              <p>Select Color For Background:</p>
+              <input
+                type="color"
+                id="color"
+                defaultValue="#ffffff"
+                onChange={(e) => setBackground(e.target.value)}
+              />
+            </div>
+            <div className="hex_code">
+              <p>{background}</p>
+            </div>
+            <button
+              onClick={() => navigator.clipboard.writeText(background)}>
+              Copy Me!
+            </button>
+          </div>
+          <div
+            className="bg">
+            <div className="text" style={{ backgroundColor: `${background}`, color: `${color}` }}>
+              <h2 id="one">Read This Message</h2>
+              <p id="two">
+                When you work for peace or any other aspect of social
+                change, there are often hardships to overcome. You must
+                believe deeply that what you are doing is right, or else you
+                may become discouraged and give up. I have found that there
+                are no easy solutions to problems involving social change.
+                When you commit yourself to creating a better world, you are
+                most likely committing yourself to a lifetime of effort.
               </p>
             </div>
-            <div className="click">
-              <div className="left_code">
-                <div className="left">
-                  <p>Select Color For Text:</p>
-                  <input
-                    type="color"
-                    id="text"
-                    defaultValue="#000000"
-                    onChange={(e) => setColor(e.target.value)}
-                  />
-                </div>
-                <div className="hex_code">
-                  <p>{color}</p>
-                </div>
-                <button onClick={() => navigator.clipboard.writeText(color)}>
-                  Copy Me!
-                </button>
-              </div>
-
-              <div className="right_code">
-                <div className="right">
-                  <p>Select Color For Background:</p>
-                  <input
-                    type="color"
-                    id="color"
-                    defaultValue="#ffffff"
-                    onChange={(e) => setBackground(e.target.value)}
-                  />
-                </div>
-                <div className="hex_code">
-                  <p>{background}</p>
-                </div>
-                <button
-                  onClick={() => navigator.clipboard.writeText(background)}>
-                  Copy Me!
-                </button>
-              </div>
-              <div
-                className="bg">
-                <div className="text" style={{ backgroundColor: `${background}`, color: `${color}` }}>
-                  <h2 id="one">Read This Message</h2>
-                  <p id="two">
-                    When you work for peace or any other aspect of social
-                    change, there are often hardships to overcome. You must
-                    believe deeply that what you are doing is right, or else you
-                    may become discouraged and give up. I have found that there
-                    are no easy solutions to problems involving social change.
-                    When you commit yourself to creating a better world, you are
-                    most likely committing yourself to a lifetime of effort.
-                  </p>
-                </div>
-                <p id="message">
-                  Can You Read The Message Above Comfortably With This
-                  Background? If Yes, Then Copy The Color Code. Else Try
-                  Changing Colors.
-                </p>
-              </div>
-            </div>
-            <div className="link">
-              <button className="btn">
-                <a href="https://github.com/HridoyHazard/BetterColor">
-                  View On Github
-                </a>
-              </button>
-            </div>
-          </div>
-        </header>
-        <div className="footer">
-          <div className="footside">
-            <h1 className="footer_name">
-              <a title="" href="#">
-                BetterColor
-              </a>
-            </h1>
-            <ul className="footer_list">
-              <li class="footer_item">
-                <a title="about" href="#">
-                  About
-                </a>
-              </li>
-              <li className="footer_item">
-                <a title="contact" href="#">
-                  Contact
-                </a>
-              </li>
-              <li class="footer_item">
-                <a title="contact" href="#">
-                  Support
-                </a>
-              </li>
-            </ul>
+            <p id="message">
+              Can You Read The Message Above Comfortably With This
+              Background? If Yes, Then Copy The Color Code. Else Try
+              Changing Colors.
+            </p>
           </div>
         </div>
-        <div class="footside2">
+        <div className="link">
+          <button className="btn">
+            <a href="https://github.com/HridoyHazard/BetterColor">
+              View On Github
+            </a>
+          </button>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <div className="footside">
+          <h1 className="footer_name">
+            <a title="" href="#">
+              BetterColor
+            </a>
+          </h1>
+          <ul className="footer_list">
+            <li class="footer_item">
+              <a title="about" href="#">
+                About
+              </a>
+            </li>
+            <li className="footer_item">
+              <a title="contact" href="#">
+                Contact
+              </a>
+            </li>
+            <li class="footer_item">
+              <a title="contact" href="#">
+                Support
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="footside">
           <p class="footer_text">Connect with us</p>
           <div class="social">
             <div className="social_icon">
@@ -165,7 +161,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </footer>
+    </>
   );
 }
